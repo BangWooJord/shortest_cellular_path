485a4d8d118e340d7a0ad9781e3e9e0212ea3a20
BangWooJord <58793833+BangWooJord@users.noreply.github.com>
2021-04-19 16:27:22 +0300

    Now i can draw using LMD and RMB

--- shortest_celluar_path/shortest_celluar_path.cpp
+++ shortest_celluar_path/shortest_celluar_path.cpp
@@ -2,9 +2,10 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <random>
+#include <thread>
 #include "../cpptimer/cpptimer.h"
 
-int generateMap(cv::Mat& map, int w, int h) {
+void generateMap(cv::Mat& map, int w, int h) {
 	map = cv::Mat(h, w, CV_8U);
 	std::random_device rd;
 	std::mt19937_64 mt(rd());
@@ -15,24 +16,43 @@ int generateMap(cv::Mat& map, int w, int h) {
 			map.at<uchar>(i, j) = (distribution(mt) == 0) ? 0 : 255;
 		}
 	}
-
-	return 0;
 }
 
-void mouseHandler(int event, int x, int y, int, void*) {
-	std::cout << x << "  " << y << std::endl; 
+void changeMap(int event) {
+}
+
+void printEvent(int event) {
+	std::cout << event << std::endl;
+}
+
+static void mouseHandler(int event, int x, int y, int flags, void* param) {
+	cv::Mat* map = static_cast<cv::Mat*> (param);
+
+	if (event == cv::EVENT_LBUTTONDOWN) {
+		map->at<uchar>(y, x) = 0;
+	}
+	else if (event == cv::EVENT_RBUTTONDOWN) {
+		map->at<uchar>(y, x) = 255;
+	}
 }
 
 int main()
 {
-	cppTimer time;
+	//cppTimer time;
 	cv::Mat main_map;
 	generateMap(main_map, 800, 600);
 
 	std::string window_name = "Window";
 	cv::namedWindow(window_name);
-	cv::imshow(window_name, main_map);
-	cv::setMouseCallback(window_name, mouseHandler, NULL);
+	//cv::imshow(window_name, main_map);
+
+	cv::setMouseCallback(window_name, mouseHandler, &main_map);
+
+	std::thread ([&]() {
+		while (true) {
+			cv::imshow(window_name, main_map);
+		}
+		}).detach();
 	cv::waitKey();
 
 	return 0;
